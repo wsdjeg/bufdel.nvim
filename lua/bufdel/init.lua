6fae8b75cd5bd8b459e1d9d49ee51a224f6c7862
Eric Wong <wsdjeg@outlook.com>
2025-11-30 23:25:25 +0800

    fix: fix bufdel

--- lua/bufdel/init.lua
+++ lua/bufdel/init.lua
@@ -1,6 +1,28 @@
 local M = {}
 
-local function delete_buf(buf, opt) end
+local function delete_buf(buf, opt)
+	if vim.o[buf].modified then
+		vim.api.nvim_echo({
+			{ 'save changes to "' .. vim.fn.bufname(buf) .. '"?  Yes/No/Cancel', "WarningMsg" },
+		}, false, {})
+		local c = vim.fn.getchar()
+		if c == 121 then
+			vim.api.nvim_buf_call(buf, function()
+				vim.cmd("write")
+			end)
+		elseif c == 110 then
+			vim.api.nvim_echo({
+				{ "discarded!", "ModeMsg" },
+			}, false, {})
+		else
+			vim.api.nvim_echo({
+				{ "canceled!", "ModeMsg" },
+			}, false, {})
+		end
+	else
+		vim.api.nvim_buf_delete(buf, {})
+	end
+end
 
 function M.delete(buf, opt)
 	if type(buf) == "number" then
