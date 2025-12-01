55b4be7455a1ad2f63998b459deead27aa2c564a
Eric Wong <wsdjeg@outlook.com>
2025-12-01 09:00:55 +0800

    feat: change alt_buf in finded windows

--- lua/bufdel/init.lua
+++ lua/bufdel/init.lua
@@ -1,26 +1,46 @@
 local M = {}
 
-local function delete_buf(buf, opt)
-	if vim.o[buf].modified then
-		vim.api.nvim_echo({
-			{ 'save changes to "' .. vim.fn.bufname(buf) .. '"?  Yes/No/Cancel', "WarningMsg" },
-		}, false, {})
-		local c = vim.fn.getchar()
-		if c == 121 then
-			vim.api.nvim_buf_call(buf, function()
-				vim.cmd("write")
-			end)
-		elseif c == 110 then
+local function lastused_buf(buffers)
+	local info = vim.fn.getbufinfo({ buflisted = 1 })
+	info = vim.tbl_filter(function(i)
+		return not vim.tbl_contains(buffers, i.bufnr)
+	end, info)
+	table.sort(info, function(a, b)
+		return a.lastused > b.lastused
+	end)
+
+	return info[1] and info[1].bufnr
+end
+
+---@param buffers table<integer>
+local function delete_buf(buffers, opt)
+    local alt_buf = lastused_buf(buffers)
+    if not alt_buf then alt_buf = vim.api.nvim_create_buf(true, false)
+	for _, buf in ipairs(buffers) do
+		if vim.o[buf].modified then
 			vim.api.nvim_echo({
-				{ "discarded!", "ModeMsg" },
+				{ 'save changes to "' .. vim.fn.bufname(buf) .. '"?  Yes/No/Cancel', "WarningMsg" },
 			}, false, {})
+			local c = vim.fn.getchar()
+			if c == 121 then
+				vim.api.nvim_buf_call(buf, function()
+					vim.cmd("write")
+				end)
+			elseif c == 110 then
+				vim.api.nvim_echo({
+					{ "discarded!", "ModeMsg" },
+				}, false, {})
+			else
+				vim.api.nvim_echo({
+					{ "canceled!", "ModeMsg" },
+				}, false, {})
+			end
 		else
-			vim.api.nvim_echo({
-				{ "canceled!", "ModeMsg" },
-			}, false, {})
+            for _, w in ipairs(vim.fn.win_findbuf(buf)) do
+                vim.api.nvim_win_set_buf(w, alt_buf)
+            end
+			vim.api.nvim_buf_delete(buf, {})
 		end
-	else
-		vim.api.nvim_buf_delete(buf, {})
 	end
 end
 
@@ -28,11 +48,13 @@ function M.delete(buf, opt)
 	if type(buf) == "number" then
 		delete_buf(buf, opt)
 	elseif type(buf) == "function" then
+        local del_bufs = {}
 		for _, v in ipairs(vim.api.nvim_list_bufs()) do
 			if buf(v) then
-				delete_buf(v, opt)
+                table.insert(del_bufs, v)
 			end
 		end
+        delete_buf(del_bufs, opt)
 	end
 end
 
